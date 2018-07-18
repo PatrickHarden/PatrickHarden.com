c344746a1008e28acf436d325c6a2e9037ae3a68
Patrick <patrick@lawyermarketingusa.com>
2018-07-18 18:42:27 -0500

    Fixed map

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -34,11 +34,10 @@ export default withSiteData(({options}) => (
     {saveCord(options)}
     <MyMapComponent
       isMarkerShown
-      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAgzgLUiRdYm4wH4xkRaqEXhK-vqMk_VSE&v=3.exp&libraries=geometry,drawing,places"
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div style={{ height: `400px` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-      
+      mapElement={<div style={{ height: `100%` }} />} 
     />
     <Container>
       <Row>
