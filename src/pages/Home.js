a635bcb955f7ada8ecec640f9d54d5c35dce8b8c
Patrick <patrick@lawyermarketingusa.com>
2018-07-19 21:29:44 -0500

    class > className

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,63 +21,63 @@ import ContactForm from '../sections/ContactForm'
 export default withSiteData(() => (
   <article id="home">
     <Helmet>
-      <body className="home" />
+      <body classNameName="home" />
     </Helmet>
-    <section class="hero">
-   <div class="container">
-      <h1 class="hero__title">I’m Patrick Harden, a <Link to="/work" class="hero__Link hero__link--second">designer</Link> and <a class="hero__link" target="_blank" href="https://github.com/PatrickHarden">web developer</a> living in Tulsa, currently working as a freelancer.</h1>
+    <section className="hero">
+   <div className="container">
+      <h1 className="hero__title">I’m Patrick Harden, a <Link to="/work" className="hero__Link hero__link--second">designer</Link> and <a className="hero__link" target="_blank" href="https://github.com/PatrickHarden">web developer</a> living in Tulsa, currently working as a freelancer.</h1>
    </div>
-   <div class="container container--wrap">
-      <div class="hero__employer hero__employer--steelseries">
-         <h3 class="employer__label">Previously:</h3>
+   <div className="container container--wrap">
+      <div className="hero__employer hero__employer--steelseries">
+         <h3 className="employer__label">Previously:</h3>
           <img id="ex3" src="images/ex3New1Logo.png" />
           
           <img id="lms" src="images/lmsLogo.png" />
           <img id="seed" src="images/SeedLogo.svg" />
       </div>
    </div>
-   <div class="hero-background">
-      <div class="rect"></div>
-      <div class="rect2"></div>
-      <div class="rect3"></div>
-      <div class="rect4"></div>
+   <div className="hero-background">
+      <div className="rect"></div>
+      <div className="rect2"></div>
+      <div className="rect3"></div>
+      <div className="rect4"></div>
    </div>
 </section>
   
-    <section id="work" class="work">
-   <div class="container">
-      <h2 class="secondary-header">Featured Work</h2>
+    <section id="work" className="work">
+   <div className="container">
+      <h2 className="secondary-header">Featured Work</h2>
    </div>
-   <div class="featured-work-container">
-   <a href="https://github.com/PatrickHarden/expect3" target="_blank" class="work-link work-link--50">
-         <div class="work-link-wrapper">
-            <img class="work-link-image" src="images/ex3screen.png" />
-            <div class="work-link-content">
-               <h3 class="link-content__header">Expect3</h3>
+   <div className="featured-work-container">
+   <a href="https://github.com/PatrickHarden/expect3" target="_blank" className="work-link work-link--50">
+         <div className="work-link-wrapper">
+            <img className="work-link-image" src="images/ex3screen.png" />
+            <div className="work-link-content">
+               <h3 className="link-content__header">Expect3</h3>
             </div>
          </div>
       </a>
-      <a href="https://github.com/PatrickHarden/React-Static-Bootstrap-Template" target="_blank" class="work-link work-link--50">
-         <div class="work-link-wrapper">
-            <img class="work-link-image" src="images/wordpressscreen.png" />
-            <div class="work-link-content">
-               <h3 class="link-content__header">React-static + Wordpress Framework</h3>
+      <a href="https://github.com/PatrickHarden/React-Static-Bootstrap-Template" target="_blank" className="work-link work-link--50">
+         <div className="work-link-wrapper">
+            <img className="work-link-image" src="images/wordpressscreen.png" />
+            <div className="work-link-content">
+               <h3 className="link-content__header">React-static + Wordpress Framework</h3>
             </div>
          </div>
       </a>
-      <a href="https://github.com/PatrickHarden/React-Static-Website-Builder" target="_blank" class="work-link work-link--50">
-         <div class="work-link-wrapper">
-            <img class="work-link-image" src="images/awbscreen2.png" />
-            <div class="work-link-content">
-               <h3 class="link-content__header">React-static Website Generator</h3>
+      <a href="https://github.com/PatrickHarden/React-Static-Website-Builder" target="_blank" className="work-link work-link--50">
+         <div className="work-link-wrapper">
+            <img className="work-link-image" src="images/awbscreen2.png" />
+            <div className="work-link-content">
+               <h3 className="link-content__header">React-static Website Generator</h3>
             </div>
          </div>
       </a>
-      <a href="https://github.com/PatrickHarden/lawyermarketingusa" target="_blank" class="work-link work-link--50">
-         <div class="work-link-wrapper">
-            <img class="work-link-image" src="images/lmsscreen.png" />
-            <div class="work-link-content">
-               <h3 class="link-content__header">Lawyer Marketing Services</h3>
+      <a href="https://github.com/PatrickHarden/lawyermarketingusa" target="_blank" className="work-link work-link--50">
+         <div className="work-link-wrapper">
+            <img className="work-link-image" src="images/lmsscreen.png" />
+            <div className="work-link-content">
+               <h3 className="link-content__header">Lawyer Marketing Services</h3>
             </div>
          </div>
       </a>
