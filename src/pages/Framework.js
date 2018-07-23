7ac0c8b2f2b3fb4f0b88dae916bb4ef2e6a03b14
Patrick <patrick@lawyermarketingusa.com>
2018-07-23 16:02:35 -0500

    Added javascript text editor

--- src/pages/Framework.js
+++ src/pages/Framework.js
@@ -2,7 +2,18 @@ import React from 'react'
 import { Container, Row, Col } from 'reactstrap';
 import { withRouteData, Link } from 'react-static'
 import {Helmet} from "react-helmet";
-//
+import { render } from 'react-dom';
+import brace from 'brace';
+import AceEditor from 'react-ace';
+
+import '../../node_modules/brace/mode/java';
+import '../../node_modules/brace/theme/monokai';
+
+function onChange(newValue) {
+  console.log('change',newValue);
+}
+
+
 
 export default () => (
   <div>
@@ -10,6 +21,7 @@ export default () => (
       <body className="framework" />
     </Helmet>
 <section>
+
    <div class="container">
    <div class="project-hero">
          <header class="hero-header">
@@ -22,7 +34,111 @@ export default () => (
         <p class="step-body mTop">React-staic excels at generating static sites from JSON data. Check out how the JSON data is pulled from wordpress and dispersed to the sub-components.</p>
      </div>
 
-     <div class="inline-image"><img src="images/Framework.png" /></div>
+    <AceEditor
+    mode="javascript"
+    theme="monokai"
+    onChange={onChange}
+    fontSize={14}
+    // showPrintMargin={true}
+    // showGutter={true}
+    highlightActiveLine={true}
+    width='100%'
+    height='1000px'
+    value={`export default {
+    getSiteData: async () => {
+      const baseURL = 'http://www.attorneytemplate.dev.php72-38.lan3-1.websitetestlink.com'
+      const { data: menus } = await axios.get(baseURL + '/index.php/wp-json/wp-api-menus/v2/menus/2')
+      const { data: options } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/options/')
+      
+    return {
+      title: 'Attorney Template',
+      siteRoot: 'https://attorneytemplate.netlify.com/',
+      siteCreator: 'Lawyer Marketing USA',
+      siteCreatorURL: 'http://lawyermarketingusa.com',
+      menus,
+      options
+    }
+  },
+  getRoutes: async () => {
+    const baseURL = 'http://www.attorneytemplate.dev.php72-38.lan3-1.websitetestlink.com'
+    const { data: pages } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/pages?per_page=99')
+    const { data: posts } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/posts?per_page=6')
+    const { data: areas } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/practice-area?per_page=99')
+    const { data: staff } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/staff?per_page=99')
+    
+    return [
+      {
+        path: '/blogs',
+        component: 'src/pages/Blog',
+        getData: () => ({
+          posts,
+        }),
+        children: posts.map(post => ({
+          path: '/{$post.slug}',
+          component: 'src/singles/Post',
+          getData: () => ({
+            post,
+          }),
+        })),
+      },
+      {
+        path: '/practice-area',
+        component: 'src/archives/AreaArchive',
+        getData: () => ({
+          areas,
+        }),
+        children: areas.map(practiceArea => ({
+          path: '/{$practiceArea.slug}',
+          component: 'src/singles/Areas',
+          getData: () => ({
+            practiceArea,
+          }),
+        })),
+      },
+      {
+        path: '/staff',
+        component: 'src/archives/StaffArchive',
+        getData: () => ({
+          staff,
+        }),
+        children: staff.map(staffMember => ({
+          path: '/{$staffMember.slug}',
+          component: 'src/singles/Staff',
+          getData: () => ({
+            staffMember,
+          }),
+        })),
+      },
+      {
+        path: '/contact',
+        component: 'src/pages/Contact',
+      },
+      {
+        path: '/',
+        component: 'src/pages/Home',
+        children: pages.map(page => ({
+        path: '/{$page.slug}',
+        component: 'src/pages/Page',
+          getData: () => ({
+            page,
+          }),
+        })),
+      },
+      {
+        is404: true,
+        component: 'src/pages/404',
+      },
+    ]
+    }
+  }
+    `}
+    setOptions={{
+      enableBasicAutocompletion: false,
+      enableLiveAutocompletion: false,
+      enableSnippets: false,
+      showLineNumbers: true,
+      // tabSize: 2,
+      }}/>
 
    </div>
          <div class="container">
@@ -48,3 +164,98 @@ export default () => (
 </section>
   </div>
 )
+
+
+//     import axios from 'axios'
+//     import React, { Component } from 'react'
+
+
+
+//     export default {
+//       getSiteData: async () => {
+//         const baseURL = 'http://www.attorneytemplate.dev.php72-38.lan3-1.websitetestlink.com'
+//         const { data: menus } = await axios.get(baseURL + '/index.php/wp-json/wp-api-menus/v2/menus/2')
+//         const { data: options } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/options/')
+
+
+//     return {
+//       title: 'Attorney Template',
+//       siteRoot: 'https://attorneytemplate.netlify.com/',
+//       siteCreator: 'Lawyer Marketing USA',
+//       siteCreatorURL: 'http://lawyermarketingusa.com',
+//       menus,
+//       options
+//     }
+//   },
+//   getRoutes: async () => {
+//     const baseURL = 'http://www.attorneytemplate.dev.php72-38.lan3-1.websitetestlink.com'
+//     const { data: pages } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/pages?per_page=99')
+//     const { data: posts } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/posts?per_page=6')
+//     const { data: areas } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/practice-area?per_page=99')
+//     const { data: staff } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/staff?per_page=99')
+
+//     return [
+//       {
+//         path: '/blogs',
+//         component: 'src/pages/Blog',
+//         getData: () => ({
+//           posts,
+//         }),
+//         children: posts.map(post => ({
+//           path: `/${post.slug}`,
+//           component: 'src/singles/Post',
+//           getData: () => ({
+//             post,
+//           }),
+//         })),
+//       },
+//       {
+//         path: '/practice-area',
+//         component: 'src/archives/AreaArchive',
+//         getData: () => ({
+//           areas,
+//         }),
+//         children: areas.map(practiceArea => ({
+//           path: `/${practiceArea.slug}`,
+//           component: 'src/singles/Areas',
+//           getData: () => ({
+//             practiceArea,
+//           }),
+//         })),
+//       },
+//       {
+//         path: '/staff',
+//         component: 'src/archives/StaffArchive',
+//         getData: () => ({
+//           staff,
+//         }),
+//         children: staff.map(staffMember => ({
+//           path: `/${staffMember.slug}`,
+//           component: 'src/singles/Staff',
+//           getData: () => ({
+//             staffMember,
+//           }),
+//         })),
+//       },
+//       {
+//         path: '/contact',
+//         component: 'src/pages/Contact',
+//       },
+//       {
+//         path: '/',
+//         component: 'src/pages/Home',
+//         children: pages.map(page => ({
+//         path: `/${page.slug}`,
+//         component: 'src/pages/Page',
+//           getData: () => ({
+//             page,
+//           }),
+//         })),
+//       },
+//       {
+//         is404: true,
+//         component: 'src/pages/404',
+//       },
+//     ]
+//   }
+// }
